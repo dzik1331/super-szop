f5a2c221136ca3d22fe0c30a41f87f812a43ccea
Krzysztof <k.nieradzik@wasko.pl>
2018-10-17 12:55:55 +0200

    dodawanie artykułu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Article} from './article/Article';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Test 1', 'http://test1.pl'),
+      new Article('Test 2', 'http://test2.pl'),
+      new Article('Test 3', 'http://test3.pl'),
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement) {
+    this.articles.push(new Article(title.value, link.value));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,29 @@
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {Article} from './Article';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'ng-row';
+  @Input() article: Article;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return true;
+  }
+
+}
